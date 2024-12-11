f7a59ae29f1361bc36e401f9d0139fc5d939007a
Kutloano <realkutloano@gmail.com>
2024-12-11 15:29:22 +0200

    Added variables to GamePanel

--- GamePanel.java
+++ GamePanel.java
@@ -1,10 +1,24 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 import javax.swing.*;
 
 public class GamePanel extends JPanel implements ActionListener {
     final int SCREEN_WIDTH = 500;
     final int SCREEN_HEIGHT = 500;
+    final int CELL_SIZE = 25;
+    final int GAME_CELLS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (CELL_SIZE * CELL_SIZE);
+    int x[] = new int[GAME_CELLS]; //Array for all possible snake x positions
+    int y[] = new int[GAME_CELLS]; //Array for all possible snake y positions
+    int DELAY = 85; //Timer delay/Game speed
+    char direction = 'R';
+    int applesEaten = 0;
+    int appleX;
+    int appleY;
+    Timer timer;
+    Random random;
+    Image forestBackground;
+    Image cityBackground;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
